0a3e7e6e2f6f297fa4b9e47f2b64b6ee20de76a8
prakhargupta31 <151626621+prakhargupta31@users.noreply.github.com>
2025-01-04 00:31:03 +0530

    Update GetUserData.java

--- src/java/com/prakhar/backend/GetUserData.java
+++ src/java/com/prakhar/backend/GetUserData.java
@@ -21,10 +21,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author Deepak
- */
+
 public class GetUserData extends HttpServlet
 {
 
