755e13b056d04f4be88de20a58e96ce5a7baba70
prakhargupta31 <151626621+prakhargupta31@users.noreply.github.com>
2025-01-04 00:28:54 +0530

    Update DownloadResume.java

--- src/java/com/prakhar/backend/DownloadResume.java
+++ src/java/com/prakhar/backend/DownloadResume.java
@@ -17,10 +17,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Deepak
- */
+
 public class DownloadResume extends HttpServlet
 {
     @Override
