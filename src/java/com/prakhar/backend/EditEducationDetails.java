6394229661415f403c698fce5295523f1df1990c
prakhargupta31 <151626621+prakhargupta31@users.noreply.github.com>
2025-01-04 00:29:48 +0530

    Update EditEducationDetails.java

--- src/java/com/prakhar/backend/EditEducationDetails.java
+++ src/java/com/prakhar/backend/EditEducationDetails.java
@@ -19,10 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author Deepak
- */
+
 public class EditEducationDetails extends HttpServlet
 {
     @Override
